3c01efae057a6119a0c411d14978958825a4f632
ParnThanatibordee <parn8946@gmail.com>
2023-05-16 22:35:33 +0900

    add movie_test.py

---
+++ tests/movie_test.py
@@ -0,0 +1,13 @@
+"""Tests of the Movie.
+
+Author: Thanatibordee Sihaboonthong
+"""
+
+import unittest
+from movierental.customer import Customer
+from movierental.rental import Rental
+from movierental.movie import Movie, PriceCode
+
+
+class MovieTest(unittest.TestCase):
+    pass
